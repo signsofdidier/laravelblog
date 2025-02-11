e50d69b5ddbd1420e31edd79601e3ba7b7c67037
signsofdidier <signsofdidier@gmail.com>
2025-02-11 13:30:47 +0100

    first upload

---
+++ resources/views/backend/users/index.blade.php
@@ -0,0 +1,7 @@
+<h1>Users werkt</h1>
+
+<ul>
+@foreach($users as $user)
+    <li>{{$user->name}}</li>
+@endforeach
+</ul>
---
+++ routes/web.php
@@ -0,0 +1,25 @@
+<?php
+
+use App\Http\Controllers\ProfileController;
+use App\Http\Controllers\UserController;
+use App\Models\User;
+use Illuminate\Support\Facades\Route;
+
+Route::get('/', function () {
+    return view('/frontend.home');
+
+});
+
+Route::resource('/backend/users', UserController::class); //geeft toegang tot alle 7 functies in UserController
+
+Route::get('/dashboard', function () {
+    return view('backend.dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
+
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
+
+require __DIR__.'/auth.php';
